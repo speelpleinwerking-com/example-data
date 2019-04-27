b6e4e07cbaf5c047897ab6ac2a30765c0048f967
Thomas Toye <thomas@toye.io>
2019-04-27 11:58:19 +0200

    Add more tests

--- test.js
+++ test.js
@@ -1,11 +1,26 @@
 const examples = require('./dist');
 const assert = require('assert');
+const models = require('@speelpleinwerking.com/protobufs')
 
-describe('playground examples', function() {
-  describe('playgrounds', function() {
+describe('playground examples', () => {
+  describe('playgrounds list', () => {
     it('should have length 3', () => {
       assert.equal(examples.playgrounds.length, 3);
     });
   });
+
+  describe('individual example playgrounds', () => {
+    it('maximumExample should pass Playground#verify', () => {
+      assert.equal(models.Playground.verify(examples.maximumExample), null);
+    });
+
+    it('minimumExample should pass Playground#verify', () => {
+      assert.equal(models.Playground.verify(examples.minimumExample), null);
+    });
+
+    it('debugExample should pass Playground#verify', () => {
+      assert.equal(models.Playground.verify(examples.debugExample), null);
+    });
+ });
 });
 
